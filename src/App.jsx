19a925ac939078057a918d1cfbfdcdec5e5e83dc
Argyris <vassilopargiris@gmail.com>
2024-09-13 17:17:52 +0300

    Hash router

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
-    </HashRouter>
+    <>
+      <HashRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
