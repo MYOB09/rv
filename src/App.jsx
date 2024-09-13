92eb632c406486159b4be1b993b20c05e462d72e
Argyris <vassilopargiris@gmail.com>
2024-09-13 17:03:23 +0300

    react-routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import HomePage from "./Pages/HomePage";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
